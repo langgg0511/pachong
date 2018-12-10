6539ac1d3521e237d5816d72bc223ebcbcf61a38
langgg0511 <38091079+langgg0511@users.noreply.github.com>
2018-12-10 13:53:08 +0800

    Delete test3.py

--- test3.py
+++
@@ -1,5 +0,0 @@
-import requests
-url = "http://61.142.33.204"
-headers = {'User-Agent' :'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
-request = requests.get(url, headers=headers)
-print(request)
